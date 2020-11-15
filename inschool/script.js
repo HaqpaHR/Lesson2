40f9e056004367e73621b8f4728e47dc0c99f851
HaqpaHR <pereverzev.viacheslav@gmail.com>
2020-11-15 11:16:03 +0200

    average

---
+++ inschool/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const app = document.querySelector('something');
+
+const answer = prompt('угадайте число?');
+
+const other = alert(String(answer));
+
+console.log(Number(answer));
+
