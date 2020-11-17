4066a20183f9bab0b436ecd7abc14e12bbc73536
HaqpaHR <pereverzev.viacheslav@gmail.com>
2020-11-17 20:04:05 +0200

    lalala

--- avg,/script.js
+++ avg,/script.js
@@ -7,11 +7,3 @@ let numbers = answer1.split(re);
 
 console.log(numbers);
 
-let num1 = parseInt(array(0));
-let num2 = parseInt(array(1));
-let num3 = parseInt(array(2));
-let num4 = parseInt(array(3));
-let num5 = parseInt(array(4));
-let len = Number(length);
-
-console.log((num1+num2+num3+num4+num5)/len);
